214ae3d7d86c0ee47759603c4cb9e46d4004562a
Gres <gres@gres.biz>
2019-02-06 19:29:26 +0300

    Formatting

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 var openedTab = null;
 
 function openMain() {
-    if (openedTab != null) {
-        chrome.tabs.update(openedTab, { highlighted: true });
-    }
-    else {
-        let url = chrome.extension.getURL('index.html');
-        chrome.tabs.create({ 'url': url }, function (tab) {
-            openedTab = tab.id;
-        });
-    }
+  if (openedTab != null) {
+    chrome.tabs.update(openedTab, { highlighted: true });
+  }
+  else {
+    let url = chrome.extension.getURL('index.html');
+    chrome.tabs.create({ 'url': url }, function (tab) {
+      openedTab = tab.id;
+    });
+  }
 }
 
 chrome.tabs.onRemoved.addListener(function (tabId) {
-    if (tabId == openedTab) {
-        openedTab = null;
-    }
+  if (tabId == openedTab) {
+    openedTab = null;
+  }
 });
 
 chrome.browserAction.onClicked.addListener(openMain);
