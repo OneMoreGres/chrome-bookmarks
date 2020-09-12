a1d198decb418307cd372fee336bd31b6e64f57a
Gres <gres@gres.biz>
2020-09-12 21:35:40 +0300

    Fix incorrect extension's tab detection

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 var openedTab = null;
+var completeCount = 0;
 
 function openMain() {
   if (openedTab != null) {
@@ -8,9 +9,19 @@ function openMain() {
     let url = chrome.extension.getURL('index.html');
     chrome.tabs.create({ 'url': url }, function (tab) {
       openedTab = tab.id;
+      completeCount = 0;
     });
   }
 }
+
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+  if (tabId == openedTab) {
+    if (change.status == "complete")
+      completeCount += 1;
+    if (completeCount > 1) // url changed? precise check requires "tabs" permission
+      openedTab = null;
+  }
+});
 
 chrome.tabs.onRemoved.addListener(function (tabId) {
   if (tabId == openedTab) {
